aa7fcc608f09d16162e8f03312ad7be641108ff1
mikestraney <emstraney@gmail.com>
2025-08-09 22:31:34 -0400

    Use pymunk physics for player

--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 import pygame
+import pymunk
+import physics
 from bullet import Bullet
 from settings import (
     WIDTH,
     PLAYER_SPEED,
     PLAYER_JUMP,
-    GRAVITY,
+    FPS,
     GROUND_LEVEL,
 )
 
@@ -17,29 +19,33 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((40, 50))
         self.image.fill((0, 255, 0))
         self.rect = self.image.get_rect(midbottom=pos)
-        self.vel_y = 0
+        mass = 1
+        moment = pymunk.moment_for_box(mass, self.rect.size)
+        self.body = pymunk.Body(mass, moment)
+        self.body.position = self.rect.center
+        self.shape = pymunk.Poly.create_box(self.body, self.rect.size)
+        self.shape.elasticity = 0.0
+        physics.space.add(self.body, self.shape)
         self.on_ground = False
         self.direction = 1
         self.last_shot = 0
         self.shoot_delay = 250  # milliseconds
 
     def handle_input(self, keys):
+        vx = 0
         if keys[pygame.K_LEFT]:
-            self.rect.x -= PLAYER_SPEED
+            vx -= PLAYER_SPEED * FPS
             self.direction = -1
         if keys[pygame.K_RIGHT]:
-            self.rect.x += PLAYER_SPEED
+            vx += PLAYER_SPEED * FPS
             self.direction = 1
+        self.body.velocity = (vx, self.body.velocity.y)
         if keys[pygame.K_UP] or keys[pygame.K_z]:
             self.jump()
 
-    def apply_gravity(self):
-        self.vel_y += GRAVITY
-        self.rect.y += self.vel_y
-
     def jump(self):
         if self.on_ground:
-            self.vel_y = -PLAYER_JUMP
+            self.body.velocity = (self.body.velocity.x, -PLAYER_JUMP * FPS)
             self.on_ground = False
 
     def shoot(self, bullets_group):
@@ -55,14 +61,18 @@ class Player(pygame.sprite.Sprite):
 
     def update(self, keys):
         self.handle_input(keys)
-        self.apply_gravity()
-        # keep within screen
-        self.rect.left = max(self.rect.left, 0)
-        self.rect.right = min(self.rect.right, WIDTH)
-        # ground collision
-        if self.rect.bottom >= GROUND_LEVEL:
-            self.rect.bottom = GROUND_LEVEL
-            self.vel_y = 0
-            self.on_ground = True
-        else:
-            self.on_ground = False
+
+    def sync_with_body(self):
+        self.rect.center = self.body.position
+        if self.rect.left < 0:
+            self.rect.left = 0
+            self.body.position = (self.rect.centerx, self.body.position.y)
+            self.body.velocity = (0, self.body.velocity.y)
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            self.body.position = (self.rect.centerx, self.body.position.y)
+            self.body.velocity = (0, self.body.velocity.y)
+        self.on_ground = (
+            abs(self.rect.bottom - GROUND_LEVEL) < 1
+            and abs(self.body.velocity.y) < 1
+        )
